09b54985557cc0ed8edd96a4d825bdcfcba3e6ea
Ryan-B-Taylor <99675957+Ryan-B-Taylor@users.noreply.github.com>
2022-03-09 13:34:12 -0500

    Add files via upload

---
+++ GroupProject.js
@@ -0,0 +1,10 @@
+/*Add Categories using AJAX 
+Categories are Pizza, wings, sides, and drinks
+*/
+
+/* Add takeout menu drag and drop functionality
+When an item is dragged over such as pizza give an alert
+box that gives a choice for the size of the pizza or toppings 
+if it is a custom pizza
+add values to each size that adds up to a total price
+*/
