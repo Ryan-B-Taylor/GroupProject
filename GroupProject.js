00cbd13171409769ac3b8bf89edbf3a88da27f3f
Ryan-B-Taylor <99675957+Ryan-B-Taylor@users.noreply.github.com>
2022-03-11 09:48:13 -0500

    Add files via upload
    AJAX implementation

--- GroupProject.js
+++ GroupProject.js
@@ -1,6 +1,42 @@
 /*Add Categories using AJAX 
-Categories are Pizza, wings, sides, and drinks
+Categories are All Items Pizzas, wings, sides, and drinks
 */
+function GetCategory(CategoryFileName)
+{
+    //local variable
+    var xmlhttp;
+
+    //create request depending on browser type
+    if ( window.XMLHttpRequest )
+    {
+        //IE7+, Firefox, Chrome, anything made after 2006 etc.
+        xmlhttp = new XMLHttpRequest;
+    }
+    else
+    {
+        //IE6 and earlier ActiveX object
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP" );
+    }
+    // set callback function to change html in the section id menucategory
+
+    xmlhttp.onreadystatechange = function ()
+    {
+        // check for good return
+        if ((xmlhttp.readyState == 4) &&
+        ( xmlhttp.status == 200))
+        {
+            // add recieved html into the menucategory section
+            document.getElementById( "menucategory").innerHTML = xmlhttp.responseText;
+        }
+    }   
+
+    //HTTP GET request
+    xmlhttp.open("GET", CategoryFileName, true);
+
+    //send request to server
+    xmlhttp.send();
+    //End of AJAX function
+}
 
 /* Add takeout menu drag and drop functionality
 When an item is dragged over such as pizza give an alert
